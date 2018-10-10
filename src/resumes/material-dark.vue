b499357f02f3b8372d381c12e7a6406e416b96a4
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2018-10-10 14:01:30 +0200

    ok without hobby

--- src/resumes/material-dark.vue
+++ src/resumes/material-dark.vue
@@ -75,7 +75,7 @@
       </div>
     </a>
 
-    <div class="item">
+    <div class="item last">
       <div class="section-headline">
         {{ lang.skills }}
       </div>
@@ -90,48 +90,30 @@
         </div>
       </div>
     </div>
-
-    <div class="item last">
-      <div v-if="person.knowledge" class="section-headline">
-        {{ lang.additionalSkills }}
-      </div>
-      <div v-if="person.knowledge" class="skill">
-        <div class="right">
-          <span>{{person.knowledge}}</span>
-        </div>
-      </div>
-    </div>
   </div>
 
   <div class="rightCol">
     <div class="title">
       <h2>{{person.name.first}} {{person.name.middle}} {{person.name.last}}</h2>
-      <div class="description">{{person.position}}</div>
-      <span class="about">{{person.about}}</span>
+      <div>{{person.position}}</div>
     </div>
 
-    <div class="section-headline experience">{{ lang.experience }}</div>
+    <div class="section-headline">{{ lang.experience }}</div>
     <div class="block" v-for="experience in person.experience" :key="experience.company">
-      <a
-      :href="experience.website">
-        <div class="block-helper"></div>
-        <h3 class="headline">{{experience.position}} - {{experience.company}}</h3>
-          <div class="subheadline">{{experience.timeperiod}}</div>
-          <p class="info">
-            {{experience.description}}
-          </p>  
-      </a>
+      <div class="block-helper"></div>
+      <h3 class="headline">{{experience.position}} - {{experience.company}}</h3>
+        <div class="subheadline">{{experience.timeperiod}}</div>
+        <p class="info">
+          {{experience.description}}
+        </p>
     </div>
     <div class="section-headline">{{ lang.education }}</div>
     <div class="block" v-for="education in person.education" :key="education.degree">
-      <a
-      :href="education.website">
-        <div class="block-helper"></div>
-        <div class="headline">{{education.degree}}</div>
-        <p class="info">
-          {{education.timeperiod}}, {{education.description}}
-        </p>
-      </a>
+      <div class="block-helper"></div>
+      <div class="headline">{{education.degree}}</div>
+      <p class="info">
+        {{education.timeperiod}}, {{education.description}}
+      </p>
     </div>
 
     <div v-if="person.projects" class="section-headline">{{ lang.projects }}</div>
@@ -158,132 +140,122 @@ export default Vue.component(name, getVueOptions(name));
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .resume {
-  font-family:'Roboto' !important;
-  background:#cccccc;
+    font-family:'Roboto' !important;
+    background:#cccccc;
 }
 a {
-  color: inherit;
-  cursor: pointer;
-  text-decoration-line: none;
-
-  &:visited {
-    color: inherit;
-  }
+    cursor:pointer;
 }
 .description-personal {
-  margin-left:20px;
-  margin-top:20px;
-  padding-right:40px;
-  text-align:justify;
-  font-family:Roboto;
+    margin-left:20px;
+    margin-top:20px;
+    padding-right:40px;
+    text-align:justify;
+    font-family:Roboto;
 }
 .title {
-  right:25px;
-  padding-left:20px;
-  padding-top:10px;
-  bottom:25px;
-  h2 {
-    text-transform:uppercase;
-    display:block;
-    font-size:1.17em;
-    -webkit-margin-before:1em;
-    -webkit-margin-after:1em;
-    -webkit-margin-start:0;
-    -webkit-margin-end:0;
-    color:white;
-    color:rgba(0,0,0,0.7);
-    padding-top:0;
-    margin-top:0;
-    letter-spacing:10px;
-    font-weight:400;
-  }
-  div {
-    margin-top:-5px;
-    margin-top:0;
-    margin:0;
-    padding:0;
-    line-height:15pt;
-    font-weight:300;
-    font-weight:500;
-    letter-spacing:2px;
-    letter-spacing:3px;
-    color:white;
-    color:#16151c;
-    color:rgba(63,61,60,0.71);
-    display:block;
-    font-size:0.67em;
-    font-size:10pt;
-    -webkit-margin-before:2.33em;
-    -webkit-margin-start:0;
-    -webkit-margin-end:0;
-    padding-top:0;
-    text-transform:uppercase;
-    opacity:0.8;
-  }
-  .description {
-    margin-top: -20px;
-    margin-botton: 20px;
-  }
+    right:25px;
+    padding-left:20px;
+    padding-top:20px;
+    bottom:25px;
+    h2 {
+        text-transform:uppercase;
+        display:block;
+        font-size:1.17em;
+        -webkit-margin-before:1em;
+        -webkit-margin-after:1em;
+        -webkit-margin-start:0;
+        -webkit-margin-end:0;
+        color:white;
+        color:rgba(0,0,0,0.7);
+        padding-top:0;
+        margin-top:0;
+        letter-spacing:10px;
+        font-weight:400;
+    }
+    div {
+        margin-top:-5px;
+        margin-top:0;
+        margin:0;
+        padding:0;
+        line-height:15pt;
+        font-weight:300;
+        font-weight:500;
+        letter-spacing:2px;
+        letter-spacing:3px;
+        color:white;
+        color:#16151c;
+        color:rgba(63,61,60,0.71);
+        display:block;
+        font-size:0.67em;
+        font-size:10pt;
+        -webkit-margin-before:2.33em;
+        -webkit-margin-start:0;
+        -webkit-margin-end:0;
+        padding-top:0;
+        text-transform:uppercase;
+        opacity:0.8;
+    }
 }
 .section-headline {
-  text-transform:uppercase;
-  font-weight:500;
-  letter-spacing:3px;
-  font-size:10pt;
-  opacity:0.8;
-  margin-left:20px;
-  margin-top:12px;
-  margin-bottom:6px;
-  color:#3f3d3c;
+    text-transform:uppercase;
+    font-weight:450;
+    letter-spacing:3px;
+    font-size:10pt;
+    opacity:0.8;
+    margin-left:16px;
+    margin-top:30px;
+    margin-bottom:16px;
+    color:#3f3d3c;
 }
 .c {
-  clear:both;
+    clear:both;
 }
 li {
-  margin:0;
-  padding:0;
-  list-style-type:none;
-  padding-top:9px;
+    margin:0;
+    padding:0;
+    list-style-type:none;
+    padding-top:9px;
 }
 ul {
-  margin:0;
-  padding:0;
-  list-style-type:none;
+    margin:0;
+    padding:0;
+    list-style-type:none;
 }
 p {
-  margin-top:0;
-  margin-bottom:25px;
-  font-family:'Roboto', sans-serif;
-  font-weight:300;
-  font-size:10pt;
-  line-height:17pt;
+    margin-top:0;
+    margin-bottom:25px;
+    font-family:'Roboto', sans-serif;
+    font-weight:300;
+    font-size:10pt;
+    line-height:17pt;
 }
 .m_box {
-  box-shadow:0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
+    box-shadow:0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
 }
 .fa, .material-icons {
-  display:inline-block;
-  font-style:normal;
-  font-weight:normal;
-  line-height:1;
-  -webkit-font-smoothing:antialiased;
-  -moz-osx-font-smoothing:grayscale;
-  font-size:26px;
+    display:inline-block;
+    font-style:normal;
+    font-weight:normal;
+    line-height:1;
+    -webkit-font-smoothing:antialiased;
+    -moz-osx-font-smoothing:grayscale;
+    font-size:26px;
 }
 h1, h3, h5, h6 {
-  font-weight:400;
-  margin:0;
+    font-weight:400;
+    margin:0;
 }
 h2 {
-  font-weight:400;
-  font-weight:500;
-  margin:0;
-  margin:0;
-  font-size:22pt;
-  line-height:37pt;
+    font-weight:400;
+    font-weight:500;
+    margin:0;
+    margin:0;
+    font-size:22pt;
+    line-height:37pt;
 }
 h4 {
-  font-weight:400;
+    font-weight:400;
   margin:0;
   font-size:12pt;
   line-height:20pt;
@@ -295,23 +267,13 @@ h4 {
   float:right;
   display:flex;
   flex-direction:column;
-  .experience {
-    margin-top: 40px;
-  }
-  .about {
-    display: block;
-    margin-top:8px !important;
-    font-weight:375;
-    font-size: 16px;
-    color:rgba(0,0,0,0.541176);
-  }
   .block {
-    width:90%;
+    width:80%;
     position:relative;
     background-color:#ffffff;
-    padding:20px;
-    margin-top:5px;
-    margin-bottom:5px;
+    padding:10px 20px;
+    margin-top:4px;
+    margin-bottom:4px;
     display:inline-block;
     box-shadow:0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
     .headline {
@@ -330,7 +292,7 @@ h4 {
       font-size:14px;
       color:rgba(0,0,0,0.870588);
       margin-bottom:0;
-      padding-top:20px;
+      padding-top:10px;
     }
     .icon {
       width:16%;
@@ -438,9 +400,6 @@ h4 {
       clear:both;
       width:97%;
       padding-top:4px;
-      span {
-        font-size: 13px;
-      }
       .left {
         float:left;
         width:10%;
@@ -482,15 +441,9 @@ h4 {
       }
     }
   }
-  .item.last{ 
-    .text {
-      border-bottom-style:none;
-      padding-bottom:0;
-    }
-
-    .section-headline {
-      margin-top: 0 !important;
-    }
+  .item.last .text {
+    border-bottom-style:none;
+    padding-bottom:0;
   }
 }
 #myselfpic {
